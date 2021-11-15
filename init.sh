7e65b80f7eecd2081fbc103a2ba477acfd64cb9b
Carter <carter.sprigings@hotmail.com>
2021-11-15 13:13:17 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,11 +1,20 @@
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 sudo chsh -s $(which zsh) $(whoami)
 
 echo $0
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
 xcode-select —-install
 
-brew install zsh
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+brew update
+
+brew install pyenv
+brew install zsh
+brew install nvm
+
+pyenv install 3.9
+nvm install 16
+
+npm i -g yarn
