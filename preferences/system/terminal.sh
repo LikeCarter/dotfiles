88a36077926d02b47a876676185c9e63142fcbf2
Carter <carter@blacklithium.com>
2023-06-16 10:04:42 -0400

    Update terminal.sh

--- preferences/system/terminal.sh
+++ preferences/system/terminal.sh
@@ -11,4 +11,12 @@ defaults write com.apple.Terminal ShowLineMarks -int 0
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
 
+# Jump To A Directory That Contains foo:
+
+# j foo
+# Jump To A Child Directory:
+# Sometimes it's convenient to jump to a child directory (sub-directory of current directory) rather than typing out the full name.
+# jc bar
+# Open File Manager To Directories (instead of jumping):
+# jo music
 brew install autojump
